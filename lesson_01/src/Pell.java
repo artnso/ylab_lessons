85b316fa274eb63cc298b0cdaf9d25eea0b6599d
artnso <art_burov@inbox.ru>
2023-03-05 18:11:15 +0300

    Оптимизировано потребление памяти

--- lesson_01/src/Pell.java
+++ lesson_01/src/Pell.java
@@ -4,15 +4,23 @@ public class Pell {
     public static void main(String[] args) throws Exception {
         try (Scanner scanner = new Scanner(System.in)) {
             int n = scanner.nextInt();
-            int[] pellNumbers = new int[n+1];
-            pellNumbers[0] = 0;
-            pellNumbers[1] = 1;
+            int pell_2 = 0;
+            int pell_1 = 1;
+            int currentPell;
+
             if (n > 1){
                 for (int i = 2; i <= n; i++){
-                    pellNumbers[i] = 2 * pellNumbers[i-1] +pellNumbers[i-2];
+                    currentPell = 2 * pell_1 + pell_2;
+                    pell_2 = pell_1;
+                    pell_1 = currentPell;
                 }
             }
-            System.out.println(pellNumbers[n]);
+
+            if (n == 0) {
+                System.out.println(pell_2);
+            } else {
+                System.out.println(pell_1);
+            }
         }
     }
 }
