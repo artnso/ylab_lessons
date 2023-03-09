813c3f621a2b5faa762f646faa0b6f7f07814722
artnso <art_burov@inbox.ru>
2023-03-09 13:11:50 +0300

    Исправлены замечания

--- lesson_01/src/Pell.java
+++ lesson_01/src/Pell.java
@@ -4,16 +4,14 @@ public class Pell {
     public static void main(String[] args) throws Exception {
         try (Scanner scanner = new Scanner(System.in)) {
             int n = scanner.nextInt();
-            int pell_2 = 0;
-            int pell_1 = 1;
-            int currentPell;
+            long pell_2 = 0;
+            long pell_1 = 1;
+            long currentPell;
 
-            if (n > 1){
-                for (int i = 2; i <= n; i++){
-                    currentPell = 2 * pell_1 + pell_2;
-                    pell_2 = pell_1;
-                    pell_1 = currentPell;
-                }
+            for (int i = 2; i <= n; i++){
+                currentPell = 2 * pell_1 + pell_2;
+                pell_2 = pell_1;
+                pell_1 = currentPell;
             }
 
             if (n == 0) {
