be51f861767507c4aba378a2f378895239e070b2
artnso <art_burov@inbox.ru>
2023-03-12 15:21:07 +0300

    Добавлены дополнительные варианты тестирования

--- lesson_02/src/SnilsValidator/SnilsValidatorTest.java
+++ lesson_02/src/SnilsValidator/SnilsValidatorTest.java
@@ -4,5 +4,7 @@ public class SnilsValidatorTest {
     public static void main(String[] args) {
         System.out.println(new SnilsValidatorImpl().validate("01468870570")); // false
         System.out.println(new SnilsValidatorImpl().validate("90114404441")); // true
+        System.out.println(new SnilsValidatorImpl().validate("abcde")); // false
+        System.out.println(new SnilsValidatorImpl().validate(null)); // false
     }
 }
